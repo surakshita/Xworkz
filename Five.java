6467a1611eb3ed3a0d984a5384fdbc698e4b6c7c
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-17 11:08:23 +0530

    java program

---
+++ Five.java
@@ -0,0 +1,32 @@
+class Five
+{
+public static void main(String[] args)
+{
+System.out.println("Details");
+name();
+college();
+course();
+onlinetraining();
+city();
+}
+public static void name()
+{
+System.out.println("NAME:surakshita");
+}
+public static void college()
+{
+System.out.println("COLLEGE:appa institute");
+}
+public static void course()
+{
+System.out.println("COURSE:computer science");
+}
+public static void onlinetraining()
+{
+System.out.println("NAME:x-workz");
+}
+public static void city()
+{
+System.out.println("CITY:b'lore");
+}
+}
