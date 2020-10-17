f488f4cd93e1bef8ddf4f03a739a523aa209ac5d
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-17 11:07:46 +0530

    java program

---
+++ Three.java
@@ -0,0 +1,14 @@
+class Three
+{
+public static void main(String[] args)
+{
+System.out.println("invoking write method three times");
+write();
+write();
+write();
+}
+public static void write()
+{
+System.out.println("x-workz");
+}
+}
