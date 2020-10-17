f8bc059d78266d0afd34bee406daf773fd80cacb
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-17 11:06:55 +0530

    java program

---
+++ Work.java
@@ -0,0 +1,17 @@
+class Work 
+{
+public static void main(String[] arg)
+{
+System.out.println("place");
+music();
+drinking();
+}
+public static void music()
+{
+System.out.println("playlist");
+}
+public static void drinking()
+{
+System.out.println("water");
+}
+}
