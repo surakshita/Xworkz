33c3539fcd81419032ba4a3f8cbd656985569e11
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-20 00:01:06 +0530

    short data type

---
+++ DataTask2.java
@@ -0,0 +1,29 @@
+public class DataTask2
+{
+public static void main(String[] args)
+{
+System.out.println("examples for short data type");
+
+short monthInYear;
+monthInYear=12;
+System.out.println(monthInYear);
+
+short areaInFeet;
+areaInFeet=3500;
+System.out.println(areaInFeet);
+
+short statesInWorld;
+statesInWorld=206;
+System.out.println(statesInWorld);
+
+short minerals;
+minerals=4000;
+System.out.println(minerals);
+
+short daysInAYear;
+daysInAYear=365;
+System.out.println(daysInAYear);
+
+}
+}  
+
