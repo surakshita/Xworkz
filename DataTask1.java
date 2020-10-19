a337f7e19d7c900e7bc22fe355f81c5b77282835
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-20 00:00:15 +0530

    byte data type

---
+++ DataTask1.java
@@ -0,0 +1,27 @@
+public class DataTask1
+{
+public static void main(String[] args)
+{
+System.out.println("examples for byte data type");
+byte shoeSize;
+shoeSize=7;
+System.out.println(shoeSize);
+
+byte preciousStones;
+preciousStones=4;
+System.out.println(preciousStones);
+
+byte alphabets;
+alphabets=26;
+System.out.println(alphabets);
+
+byte herbsNumbers;
+herbsNumbers=100;
+System.out.println(herbsNumbers);
+
+byte fingers;
+fingers=10;
+System.out.println(fingers);
+
+}
+}
